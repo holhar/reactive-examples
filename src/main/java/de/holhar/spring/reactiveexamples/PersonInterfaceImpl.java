64cf4afcc03981dc160e9f1044624d5759acaf8e
Holger Harms <holhar@posteo.de>
2021-12-18 21:24:31 +0100

    Implement reactive repository

--- src/main/java/de/holhar/spring/reactiveexamples/PersonInterfaceImpl.java
+++ src/main/java/de/holhar/spring/reactiveexamples/PersonInterfaceImpl.java
@@ -5,13 +5,19 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PersonInterfaceImpl implements PersonInterface {
+
+    Person michael = new Person(1, "Michael", "Weston");
+    Person fiona = new Person(2, "Fiona", "Glenanne");
+    Person sam = new Person(3, "Sam", "Axe");
+    Person jesse = new Person(4, "Jesse", "Porter");
+
     @Override
     public Mono<Person> getById(Integer id) {
-        return null;
+        return Mono.just(michael);
     }
 
     @Override
     public Flux<Person> findAll() {
-        return null;
+        return Flux.just(michael, fiona, sam, jesse);
     }
 }
