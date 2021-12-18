f33fedffabd3a5a9254d370cea3be087e7475ca4
Holger Harms <holhar@posteo.de>
2021-12-18 21:16:43 +0100

    initial commit

---
+++ src/main/java/de/holhar/spring/reactiveexamples/ReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package de.holhar.spring.reactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/holhar/spring/reactiveexamples/ReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.holhar.spring.reactiveexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
